07b23ff4165fb913645f9c9370451a79c021ab96
Sap <ssspandey27@gmail.com>
2025-06-29 20:30:46 +0530

    jxs file modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       
-      <h1>Hello From Jenkins</h1>
+      <h1>Hello From Jenkins 1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
