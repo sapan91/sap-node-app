7328260c80a9650e9039a5e6fa2f501c53ebb7af
Sap <ssspandey27@gmail.com>
2025-06-29 20:28:11 +0530

    Added jenkins file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1>Hello From Jenkins</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
